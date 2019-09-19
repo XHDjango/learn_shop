ad72c2ac9de2c413087351756d1e3611a98df7ba
lamborghini1993 <1323242382@qq.com>
2019-09-19 11:28:25 +0800

    70-4-添加模型并插入数据

--- Shop/models.py
+++ Shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Shop(models.Model):
+    nid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=32)
+    img_url = models.CharField(max_length=256)
+
+    class Meta:
+        db_table = "shop"
