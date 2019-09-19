97196fff4b268f3ca07dd1b202bc8c14964e3377
lamborghini1993 <1323242382@qq.com>
2019-09-19 16:23:15 +0800

    71-删除shop表相关

--- Shop/models.py
+++ Shop/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-
-class Shop(models.Model):
-    nid = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=32)
-    img_url = models.CharField(max_length=256)
-
-    class Meta:
-        db_table = "shop"
-
 class Car(models.Model):
     type = models.CharField(max_length=16)
     name = models.CharField(max_length=32)
--- Shop/views.py
+++ Shop/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Shop, Car
+
+from .models import Car
 
 
 # Create your views here.
